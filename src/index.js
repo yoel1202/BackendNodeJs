51dc5cf578f3f5c1512b82ead1c0302976f28de3
Yoel Cerdas <yoel_cerdas@bhd.com.do>
2023-05-20 07:59:35 -0600

    feature(pruebas) integracion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from "./app";
 
 const main = () => {
-    app.get('/',(req,res)=> res.json({"api":"BIENVENIDOS A LA API :)"}))
+    app.get('/',(req,res)=> res.json({"api":"BIENVENIDOS A LA API v2:)"}))
     app.listen( process.env.PORT || 9090);
 
 };
